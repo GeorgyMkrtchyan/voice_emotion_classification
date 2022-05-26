16d5b495959c7c29296d4f30d46df97e870fd89f
nevermindoleg <nevermindoleg@mail.ru>
2022-05-27 02:59:32 +0300

    add Conformer_Classifier.py. Did not run it.

---
+++ codes/Conformer_Classifier.py
@@ -0,0 +1,24 @@
+import torch
+from openspeech.encoders import OpenspeechEncoder
+from torch import nn
+
+from codes import BaseClassificationModel
+
+
+class Conformer_Classifier(BaseClassificationModel):
+    def init(self, input_dims=93, context_dims=12):
+        super().init()
+        self.encoder = OpenspeechEncoder(num_classes=10, input_dims=93, joint_ctc_attention=False)
+        classifier_in_dims = 512 + context_dims
+        self.classifier = nn.Sequential(
+            nn.Linear(classifier_in_dims, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+
+def forward(self, x: torch.Tensor):
+    x, _ = self.encoder(x)
+    x = torch.cat([x.mean(1), context], 1)
+    x = self.classifier(x)
+    return x
