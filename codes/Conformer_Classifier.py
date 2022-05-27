9f43d0403f1c74389815057a198a5bc26487a519
nevermindoleg <nevermindoleg@mail.ru>
2022-05-27 03:21:56 +0300

    fixing mistake

--- codes/Conformer_Classifier.py
+++ codes/Conformer_Classifier.py
@@ -1,14 +1,15 @@
 import torch
-from openspeech.encoders import OpenspeechEncoder
+from openspeech.encoders import OpenspeechEncoder, ConformerEncoder
 from torch import nn
 
 from codes import BaseClassificationModel
 
 
 class Conformer_Classifier(BaseClassificationModel):
-    def init(self, input_dims=93, context_dims=12):
-        super().init()
-        self.encoder = OpenspeechEncoder(num_classes=10, input_dims=93, joint_ctc_attention=False)
+
+    def __init__(self, input_dims=93, context_dims=12):
+        super().__init__()
+        self.encoder = ConformerEncoder(num_classes=10, input_dim=input_dims, joint_ctc_attention=False)
         classifier_in_dims = 512 + context_dims
         self.classifier = nn.Sequential(
             nn.Linear(classifier_in_dims, 128),
